3fb000aef2df90fe8b0c3726f8f8b1632bec5064
LolaSi <72922569+LolaSi@users.noreply.github.com>
2021-03-25 23:14:17 +0200

    first commit
    initial XAML file with file explorer button and scrollbar

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+namespace EX2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpen_Click(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine(e.Source.ToString());
+            OpenFileDialog openDialog = new OpenFileDialog();
+            openDialog.Multiselect = true;
+            openDialog.Filter = "Log Files|* .log|Textfiles|.txt|All files|.*";
+            openDialog.DefaultExt = ".log";
+            Nullable<bool> dialogOK = openDialog.ShowDialog();
+            if (dialogOK == true)
+            {
+                string fileNames = "";
+                foreach (string fileName in openDialog.FileNames)
+                {
+                    fileNames += ";" + fileName;
+                }
+                fileNames = fileNames.Substring(1);
+                Console.WriteLine(fileNames);
+            }
+        }
+    }
+}
