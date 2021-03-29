a6d2a95f949e66643d6af2e156b15eded5e6c027
Dark-Leader <53357564+Dark-Leader@users.noreply.github.com>
2021-03-29 22:58:05 +0300

    temp viewModel
    temporary viewModel to connect fields with data binding
    
    NOT FINAL!

---
+++ timeElapsed.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex1
+{
+    public class timeElapsed : INotifyPropertyChanged
+    {
+        private string time;
+        public event PropertyChangedEventHandler PropertyChanged;
+        public string Time
+        {
+            get
+            {
+                return time;
+            }
+            set
+            {
+                if (time != value)
+                {
+                    time = value;
+                    PropertyChangedEventHandler handler = PropertyChanged;
+                    if (handler != null)
+                    {
+                        handler(this, new PropertyChangedEventArgs("timeElapsed"));
+                    }
+                }
+            }
+        }
+
+        public timeElapsed()
+        {
+            this.time = "00:00:00";
+        }
+    }
+}
