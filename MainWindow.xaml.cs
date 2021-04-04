c24caa0cfed5b346194922fc54fcc183e45bed2b
DarkLe3der <amnb9@walla.com>
2021-04-04 23:56:53 +0300

    Amit - bug fixes and added viewModel
    (cherry picked from commit ed85e85840886ffc546b440f3abd0411342fdab2)
    
    # Conflicts:
    #	MainWindow.xaml.cs
    #	viewModel.cs

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,23 +1,9 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-//using ex1;
 using Microsoft.Win32;
-using System.Threading;
 
-// project ex2
-// done 3/4/2021
+
 namespace EX2
 {
     /// <summary>
@@ -25,47 +11,45 @@ namespace EX2
     /// </summary>
     public partial class MainWindow : Window
     {
-        //private timeElapsed tm;
+        private viewModel vm;
         public MainWindow()
         {
             InitializeComponent();
             // Example for injecting 15 buttons into the stackPanel
-            //tm = new timeElapsed();
-            //this.DataContext = tm;
-
-            // opens flight gear
-            string csvData = "D:\\Learn It\\2nd year\\advanced programming2\\project\\reg_flight.csv";
-            FlightSimulator fs = new FlightSimulator(csvData);
-            Thread t = new Thread(new ThreadStart(fs.play));
-            t.Start();
-
-            //
-
-            int num = 15; // need to get num of buttons from model.
-            for (int i = 0; i < num; i++)
+            vm = new viewModel();
+            this.DataContext = vm;
+            foreach (string var in this.vm.Variables)
             {
-                System.Windows.Controls.Button button = new Button();
-                {
-                    button.Content = i.ToString();
-                    button.Name = "Button" + i.ToString();
-                    button.Click += new RoutedEventHandler(stackButton_Click); // set the click button to stackButton_Click
-                }
+                Button button = new Button();
+                button.Content = var;
+                button.Name = "Button_" + var;
+                button.Click += new RoutedEventHandler(stackButton_Click);
                 sp.Children.Add(button);
             }
+
+
         }
         // default function to handle all stackPanel button clicks.
         private void stackButton_Click(object sender, RoutedEventArgs e)
         {
             Button b = (Button)sender;
-            Console.WriteLine(b.Content.ToString());
+            Console.WriteLine(b.Name);
+            //this.vm.StackButton_string = b.Name;
+
+
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender">  </param>
+        /// <param name="e"></param>
         private void btnOpen_Click(object sender, RoutedEventArgs e)
         {
-            Console.WriteLine(e.Source.ToString());
+
             OpenFileDialog openDialog = new OpenFileDialog();
             openDialog.Multiselect = true;
-            openDialog.Filter = "Log Files|* .log|Textfiles|.txt|All files|.*";
+            openDialog.Filter = "Log Files|* .log|Textfiles|*.txt|All files|*.*";
             openDialog.DefaultExt = ".log";
             Nullable<bool> dialogOK = openDialog.ShowDialog();
             if (dialogOK == true)
@@ -77,12 +61,22 @@ namespace EX2
                 }
                 fileNames = fileNames.Substring(1);
                 Console.WriteLine(fileNames);
+                this.vm.Open_file = fileNames;
             }
+
+
+
         }
+
         // TODO: send the model and the flightgear simulator the current value of the slider.
+        //Slider.
         private void SliderValue_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
-            return;
+            if (IsLoaded)
+            {
+                Console.WriteLine(SliderValue);
+            }
+
         }
     }
 }
---
+++ viewModel.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections.Generic;
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+
+namespace EX2
+{
+    public partial class viewModel
+    {
+        //private List<ChartData> points = new List<ChartData>();
+        private List<String> variables = new List<String>();
+        private string time;
+        private double playback_speed;
+
+
+        private string open_file;// works for both csv file and .exe file.
+        private string stackButton_string;
+        public viewModel()
+        {
+            this.variables.Add("speed");
+            this.variables.Add("height");
+            this.variables.Add("throttle");
+            this.variables.Add("width");
+            this.variables.Add("HAHAHAHAHHAHA");
+            this.variables.Add("speed");
+            this.variables.Add("height");
+            this.variables.Add("throttle");
+            this.variables.Add("width");
+            this.variables.Add("HAHAHAHAHHAHA");
+            this.variables.Add("speed");
+            this.variables.Add("height");
+            this.variables.Add("throttle");
+            this.variables.Add("width");
+            this.variables.Add("HAHAHAHAHHAHA");
+            this.time = "00:00:00";
+            this.playback_speed = 1.0;
+
+        }
+        public string Open_file
+        {
+            get
+            {
+                return open_file;
+            }
+            set
+            {
+                if (value != open_file)
+                {
+                    open_file = value;                 
+                }
+            }
+        }
+
+        public string StackButton_string
+        {
+            get
+            {
+                return stackButton_string;
+            }
+            set
+            {
+                if (value != stackButton_string)
+                {
+                    stackButton_string = value;
+                }
+            }
+        }
+
+     public List<String> Variables
+        {
+            get
+            {
+                return variables;
+            }
+            set
+            {
+                if (value != variables)
+                {
+                    variables = value;
+                }
+            }
+        }
+        public string Time
+        {
+            get
+            {
+                return time;
+            }
+            set
+            {
+                if (value != time)
+                {
+                    time = value;
+                }
+            }
+        }
+
+        public double Playback_speed
+        {
+            get
+            {
+                return playback_speed;
+            }
+            set
+            {
+                if (value != playback_speed)
+                {
+                    playback_speed = value;
+                }
+            }
+        }
+
+
+
+
+    }
+}
