2f37513226051c238d5af956abc2e8c1bb352fce
Lola Sirota <lola.siross@gmail.com>
2021-04-04 18:11:52 +0300

    Lola - Fixed bug in the play method

--- FlightSimulator.cs
+++ FlightSimulator.cs
@@ -27,18 +27,21 @@ namespace EX2
 
 
             // command line arguments to pass to the application
-            private string arguments = "--generic=socket,in,10,127.0.0.1,5400,tcp,";
+            private string arguments = "--generic=socket,in,10,127.0.0.1,5400,tcp,playback_small --fdm=null";
 
+            /*
             public FlightGear(string fileName, string settings)
             {
                 this.fileName = fileName;
-                arguments = arguments + settings + " --fdm=null";
-                //arguments = arguments + settings;
+
+                // ADD path to settings and add it to data/protocol in fg and to args
+                //arguments = arguments + settings + " --fdm=null";
+                
             }
 
             public void start()
             {
-                //System.Diagnostics.Process.Start(fileName, arguments);
+                // opens the application
 
                 ProcessStartInfo startInfo = new ProcessStartInfo(fileName);
 
@@ -50,21 +53,18 @@ namespace EX2
 
             }
         }
-        */
-        
+            */
+
+
         public FlightSimulator(string data)
         {
             string pathToApp = "D:\\Applications\\FlightGear 2020.3.6\\bin\\fgfs.exe";
             string pathToSettings = "D:\\Learn It\\2nd year\advanced programming2\\project\\playback_small.xml";
 
-
-            //FlightGear fg = new FlightGear(pathToApp, pathToSettings);
-            //fg.start();
-
             this.csvData = data;
-            
-            
-            
+
+            // FlightGear fg = new FlightGear(pathToApp, pathToSettings);
+            // fg.start();             
         }
 
         public void play()
@@ -74,7 +74,9 @@ namespace EX2
                 IPAddress ip = IPAddress.Parse("127.0.0.1");
                 int port = 5400;
                 TcpClient client = new TcpClient();
+
                 client.Connect(ip, port);
+                Console.WriteLine("Connection established");
                 NetworkStream ns = client.GetStream();
 
 
@@ -83,15 +85,32 @@ namespace EX2
                 using (StreamReader rd = new StreamReader(this.csvData))
                 {
                     String line;
-                    
-                    while ((line=rd.ReadLine()) != null)
+
+                    while ((line = rd.ReadLine()) != null)
                     {
-                        byte[] buffer = Encoding.ASCII.GetBytes(line + "/r/n");
-                        ns.Write(buffer, 0, buffer.Length);
-                        Thread.Sleep(100);
-                        
-                    }  
+                        line += "\r\n";
+                        Console.WriteLine(line);
+                        if (ns.CanWrite)
+                        {
+                            byte[] buffer = Encoding.ASCII.GetBytes(line);
+                            ns.Write(buffer, 0, buffer.Length);
+                            Thread.Sleep(100);
+                        }
+                        else
+                        {
+                            Console.WriteLine("You cannot write data to this stream.");
+                            /*
+                            tcpClient.Close();
+
+                            // Closing the tcpClient instance does not close the network stream.
+                            netStream.Close();
+                            return;
+                            */
+                        }
+
+                    }
                 }
+
             }
             catch (ArgumentNullException e)
             {
