88a2547ab43db02a24d43f8019eaf0b0d64d22c7
Lola Sirota <lola.siross@gmail.com>
2021-04-05 15:11:25 +0300

    Lola - created interface for tcp client

---
+++ ITelnetClient.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX2
+{
+    /// <summary>
+    /// Provides a mechanism for basic tcp protocol to send data over
+    /// </summary>
+    interface ITelnetClient
+    {
+        /// <summary>
+        /// Connect to desired server in specified ip and port
+        /// </summary>
+        /// <param name="ip">The ip of the server.</param>
+        /// <param name="port">The port that the server.</param>
+        void connect(string ip, int port);
+
+        /// <summary>
+        /// Sends the specified data
+        /// </summary>
+        /// <param name="data">Data to send over</param>
+        void write(string data);
+
+        /// <summary>
+        /// Terminate connection.
+        /// </summary>
+        void disconnect();
+    }
+}
